29654d18724d4a5f703acf45eb49c29ce0ded0ac
decryptable <hello@decryptable.dev>
2025-05-28 12:13:47 +0000

    Update version to 1.0.2-alpha, restructure package exports, and enhance documentation

--- tsup.config.js
+++ tsup.config.js
@@ -4,12 +4,17 @@ export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs", "iife"],
   globalName: "AllDownloader",
-  splitting: true,
-  sourcemap: false,
+  // splitting: true,
+  // sourcemap: false,
   minify: true,
   dts: true,
   clean: true,
   outDir: "dist",
-  target: "esnext",
-  legacyOutput: true,
+  // target: "esnext",
+  // legacyOutput: true,
+  outExtension({ format }) {
+    return {
+      js: `.${format === 'cjs' ? 'cjs' : format === 'esm' ? 'mjs' : 'iife.js'}`,
+    };
+  },
 })
